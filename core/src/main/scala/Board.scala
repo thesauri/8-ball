b13ab02eba0906d3be7102b88d00f3810e384eaf
Walter Berggren <walter.berggren@gmail.com>
2016-03-17 17:43:49 +0200

    Added the scale parameter to the render method

--- core/src/main/scala/Board.scala
+++ core/src/main/scala/Board.scala
@@ -10,9 +10,12 @@ class Board extends Rectangle with Renderable {
   var width = 2.54
   var height = 1.27
   
-  def render(renderer: ShapeRenderer) = {
+  def render(renderer: ShapeRenderer, scale: Double) = {
     renderer.setColor(Styles.BoardColor)
     renderer.set(ShapeType.Filled)
-    renderer.rect(x.toFloat, y.toFloat, width.toFloat, height.toFloat)
+    renderer.rect(x.toFloat * scale.toFloat,
+                  y.toFloat * scale.toFloat,
+                  width.toFloat * scale.toFloat,
+                  height.toFloat * scale.toFloat)
   }
 }
