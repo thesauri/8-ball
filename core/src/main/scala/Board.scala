e77b453f17f4a3dade28e0df177f06433b5211c0
Walter Berggren <walter.berggren@gmail.com>
2016-03-17 17:19:07 +0200

    Implemented the render method for the Board class

--- core/src/main/scala/Board.scala
+++ core/src/main/scala/Board.scala
@@ -1,8 +1,15 @@
 package com.walter.eightball
 
-class Board extends Rectangle {
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+
+class Board extends Rectangle with Renderable {
+  
   var x = 0.0
   var y = 0.0
   var width = 2.54
   var height = 1.27
+  
+  def render(renderer: ShapeRenderer) = {
+    renderer.rect(x.toFloat, y.toFloat, width.toFloat, height.toFloat)
+  }
 }
