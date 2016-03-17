7391d1a30b08f72d4750d5c6fef88fd89095d548
Walter Berggren <walter.berggren@gmail.com>
2016-03-17 17:25:31 +0200

    Added a game board to the game class

--- core/src/main/scala/Eightball.scala
+++ core/src/main/scala/Eightball.scala
@@ -10,6 +10,8 @@ class Eightball extends Game {
     val camera = new OrthographicCamera(Gdx.graphics.getWidth.toFloat, Gdx.graphics.getHeight.toFloat)
     val shapeRenderer = new ShapeRenderer()
     shapeRenderer.setProjectionMatrix(camera.combined)
+    
+    val gameBoard = new Board()
   
     override def create() {}
     
