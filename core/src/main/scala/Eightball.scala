62fea17fb9b94ff989f507fea35a817242611d76
Walter Berggren <walter.berggren@gmail.com>
2016-03-17 17:23:03 +0200

    Added a camera and shaperenderer to the eightball class

--- core/src/main/scala/Eightball.scala
+++ core/src/main/scala/Eightball.scala
@@ -1,7 +1,19 @@
 package com.walter.eightball
 
 import com.badlogic.gdx.Game
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.Gdx
 
 class Eightball extends Game {
+  
+    val camera = new OrthographicCamera(Gdx.graphics.getWidth.toFloat, Gdx.graphics.getHeight.toFloat)
+    val shapeRenderer = new ShapeRenderer()
+    shapeRenderer.setProjectionMatrix(camera.combined)
+  
     override def create() {}
+    
+    override def render() {
+      
+    }
 }
