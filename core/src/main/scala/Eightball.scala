06be309e5a6b34731308d298287089045a0dacd9
Walter Berggren <walter.berggren@gmail.com>
2016-03-23 12:23:17 +0200

    Add return type to methods

--- core/src/main/scala/Eightball.scala
+++ core/src/main/scala/Eightball.scala
@@ -8,22 +8,22 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType
 import com.badlogic.gdx.graphics.GL20
 
 class Eightball extends Game {
-  
+
     lazy val camera = new OrthographicCamera(Gdx.graphics.getWidth.toFloat, Gdx.graphics.getHeight.toFloat)
     lazy val shapeRenderer = new ShapeRenderer()
-    
+
     lazy val gameBoard = new Board()
-  
-    override def create() {
+
+    override def create(): Unit = {
       camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0f)
       camera.update()
       shapeRenderer.setProjectionMatrix(camera.combined)
     }
-    
-    override def render() {
+
+    override def render(): Unit = {
       Gdx.gl.glClearColor(1, 1, 1, 1);
       Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-      
+
       shapeRenderer.begin(ShapeType.Filled)
       gameBoard.render(shapeRenderer, Gdx.graphics.getWidth / 3d) //Divided by the approx. board width in meters
       shapeRenderer.end()
