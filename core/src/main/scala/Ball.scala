31868cd764c3b65b777b0cb33b01c4aa47717be8
Walter Berggren <walter.berggren@gmail.com>
2016-03-23 12:33:14 +0200

    Implemented render method for the ball

--- core/src/main/scala/Ball.scala
+++ core/src/main/scala/Ball.scala
@@ -1,6 +1,17 @@
 package com.walter.eightball
 
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType
+
 /** Represents a ball on the pool table */
-class Ball(var x: Double, var y: Double, val number: Int) extends Shape {
-  
+class Ball(var x: Double, var y: Double, val number: Int) extends Shape with Renderable {
+
+  val radius = 0.028575f //Official billiard ball dimensions in meters
+
+  def render(renderer: ShapeRenderer, scale: Double): Unit = {
+    renderer.setColor(Styles.BallColor)
+    renderer.set(ShapeType.Filled)
+    renderer.circle(x * scale, y * scale, radius * scale)
+  }
+
 }
