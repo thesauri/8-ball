e67dcc971169627bcc77f2d226d613ed44d3d241
Walter Berggren <walter.berggren@gmail.com>
2016-03-23 12:30:08 +0200

    Added a temporary generic color for the balls

--- core/src/main/scala/Styles.scala
+++ core/src/main/scala/Styles.scala
@@ -3,7 +3,8 @@ package com.walter.eightball
 import com.badlogic.gdx.graphics.Color
 
 object Styles {
-  
+
+  def BallColor: Color = Color.valueOf("#ECF0F1FF")
   def BoardColor: Color = Color.valueOf("27AE60FF")
-  
+
 }
