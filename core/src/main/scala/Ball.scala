2ba247b6f77772d5281084db77e6688f8a90dfb4
Walter Berggren <walter.berggren@gmail.com>
2016-04-12 19:32:27 +0300

    Further description of implementations

--- core/src/main/scala/Ball.scala
+++ core/src/main/scala/Ball.scala
@@ -3,7 +3,10 @@ package com.walter.eightball
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType
 
-/** Represents a ball on the pool table */
+/** Represents a ball on the pool table
+ *  
+ *  Balls implement 2D vectors that define their position, Shapes to give them
+ *  a width and height, and Renderable to handle the rendering when called. */
 class Ball(var x: Float, var y: Float, val number: Int) extends Vector2D with Shape with Renderable {
 
   val radius = 0.028575f //Official billiard ball dimensions in meters
