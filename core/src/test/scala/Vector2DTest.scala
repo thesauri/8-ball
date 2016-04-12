6ddb5ac5309b429fbec8ed020c855e6c2dc72c7c
Walter Berggren <walter.berggren@gmail.com>
2016-04-12 19:39:07 +0300

    Test for adding two vectors

--- core/src/test/scala/Vector2DTest.scala
+++ core/src/test/scala/Vector2DTest.scala
@@ -4,4 +4,9 @@ import org.scalatest._
 
 class Vector2DTest extends FlatSpec with Matchers {
 
+  "+" should "return the sum of two vectors" in {
+    val v = Vector2D(2f, 3f) + Vector2D(10f, 2f)
+    v should be (Vector2D(12f, 5f))
+  }
+  
 }
