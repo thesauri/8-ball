621e7922dd3cec6614c3d493a31f1a8a4f8d3913
Walter Berggren <walter.berggren@gmail.com>
2016-04-13 16:08:39 +0300

    Remove # from color

--- core/src/main/scala/Styles.scala
+++ core/src/main/scala/Styles.scala
@@ -4,7 +4,7 @@ import com.badlogic.gdx.graphics.Color
 
 object Styles {
 
-  def BallColor: Color = Color.valueOf("#ECF0F1FF")
+  def BallColor: Color = Color.valueOf("ECF0F1FF")
   def BoardColor: Color = Color.valueOf("27AE60FF")
 
 }
