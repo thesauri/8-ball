a15f85e315e707d78b55af97e27debd39b0399a7
Walter Berggren <walter.berggren@gmail.com>
2016-04-14 11:37:31 +0300

    Test that moveBall actually moves balls

--- core/src/test/scala/PhysicsHandlerTest.scala
+++ core/src/test/scala/PhysicsHandlerTest.scala
@@ -4,6 +4,32 @@ import org.scalatest._
 
 class PhysicsHandlerTest extends FlatSpec with Matchers {
   
+  "moveBalls" should "move the balls" in {
+    val balls = Vector(new Ball(0f, 0f, 1), new Ball(-10f, 5f, 2))
+    balls(0).velocity = Vector2D(1f, 1f)
+    balls(1).velocity = Vector2D(-5f, 0f)
+
+    //Should not have moved before called
+    balls(0).x should be (0f)
+    balls(0).y should be (0f)
+    balls(1).x should be (-10f)
+    balls(1).y should be (5f)
+    
+    //Move once with a timestep of 1 s (1s movement)
+    PhysicsHandler.moveBalls(balls, 1f)
+    balls(0).x should be (1f)
+    balls(0).y should be (1f)
+    balls(1).x should be (-15f)
+    balls(1).y should be (5f)
+    
+    //Move them 9 more times (=> total of 10s movement)
+    (0 until 9).foreach(x => PhysicsHandler.moveBalls(balls, 1f))
+    balls(0).x should be (10f)
+    balls(0).y should be (10f)
+    balls(1).x should be (-60f)
+    balls(1).y should be (5f)
+  }
+  
   "moveBalls" should "not move the balls if their velocity is 0" in {
     val balls = Vector(new Ball(0f, 0f, 1), new Ball(-10f, 5f, 2))
     PhysicsHandler.moveBalls(balls, 1f)
