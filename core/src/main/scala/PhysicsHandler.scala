fe30a90d36bbd308bd2f39d4d6c6f30ec0ad65db
Walter Berggren <walter.berggren@gmail.com>
2016-04-14 11:24:02 +0300

    Spec for a method to move balls

--- core/src/main/scala/PhysicsHandler.scala
+++ core/src/main/scala/PhysicsHandler.scala
@@ -2,4 +2,9 @@ package com.walter.eightball
 
 object PhysicsHandler {
   
+  /** Moves the given balls according to their velocities */
+  def moveBalls(balls: Seq[Ball]): Unit = {
+    ???
+  }
+  
 }
