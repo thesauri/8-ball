fd0384c4f5b2083b088a3f6b4b2b8ce4c881ee32
Walter Berggren <walter.berggren@gmail.com>
2016-04-14 11:31:29 +0300

    Add time since last execution parameter to moveBalls

--- core/src/main/scala/PhysicsHandler.scala
+++ core/src/main/scala/PhysicsHandler.scala
@@ -2,8 +2,11 @@ package com.walter.eightball
 
 object PhysicsHandler {
   
-  /** Moves the given balls according to their velocities */
-  def moveBalls(balls: Seq[Ball]): Unit = {
+  /** Moves the given balls according to their velocities
+   *  
+   *  @param the balls to move
+   *  @param time since last execution (in seconds) */
+  def moveBalls(balls: Seq[Ball], t: Float): Unit = {
     ???
   }
   
