1e79de030616e1518ec3f6077c9aacf874c35047
Walter Berggren <walter.berggren@gmail.com>
2016-04-14 11:31:59 +0300

    Add time since last execution parameter to moveBalls method calls

--- core/src/test/scala/PhysicsHandlerTest.scala
+++ core/src/test/scala/PhysicsHandlerTest.scala
@@ -6,12 +6,12 @@ class PhysicsHandlerTest extends FlatSpec with Matchers {
   
   "moveBalls" should "not move the balls if their velocity is 0" in {
     val balls = Vector(new Ball(0f, 0f, 1), new Ball(-10f, 5f, 2))
-    PhysicsHandler.moveBalls(balls)
+    PhysicsHandler.moveBalls(balls, 1f)
     balls(0).x should be (0f)
     balls(0).y should be (0f)
     balls(1).x should be (-10f)
     balls(1).y should be (5f)
-    (0 to 1000).foreach(x => PhysicsHandler.moveBalls(balls))
+    (0 to 1000).foreach(x => PhysicsHandler.moveBalls(balls, 1f))
     balls(0).x should be (0f)
     balls(0).y should be (0f)
     balls(1).x should be (-10f)
