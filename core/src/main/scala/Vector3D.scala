08e8104faef7f234f84b3680a6bc2d210726ee76
Walter Berggren <walter.berggren@gmail.com>
2016-04-19 17:59:26 +0300

    Rename asUnitVector to normalized

--- core/src/main/scala/Vector3D.scala
+++ core/src/main/scala/Vector3D.scala
@@ -24,12 +24,12 @@ trait Vector3D {
   /** Returns the vector multiplied with a constant */
   def *(c: Float): Vector3D = Vector3D(x * c, y * c, z * c)
   
-  /** Returns the vector's unit vector */
-  def asUnitVector: Vector3D = ???
-  
   /** Returns the cross product between this and another vector */
   def cross(v: Vector3D): Vector3D =
     Vector3D(y*v.z - z*v.y, z*v.x - x*v.z, x*v.y - y*v.x)
+    
+  /** Returns a normalized version of the vector */
+  def normalized: Vector3D = ???
   
   override def equals(v: Any): Boolean = v match {
     case v: Vector3D => (x == v.x) && (y == v.y) && (z == v.z)
