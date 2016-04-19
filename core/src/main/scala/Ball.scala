ba1d77e5a1f5629243e486b739ead4d9f6f481f8
Walter Berggren <walter.berggren@gmail.com>
2016-04-19 14:03:00 +0300

    Add angular velocity

--- core/src/main/scala/Ball.scala
+++ core/src/main/scala/Ball.scala
@@ -12,6 +12,7 @@ class Ball(var x: Float, var y: Float, var z: Float, val number: Int) extends Ve
   val mass = 0.16f //In kg according to the WPA spec
   val radius = 0.028575f //In m according to the WPA spec
   var velocity = Vector3D(0f, 0f, 0f)
+  var angularVelocity = Vector3D(0f, 0f, 0f)
   
   def render(renderer: ShapeRenderer, scale: Float): Unit = {
     renderer.setColor(Styles.BallColor)
