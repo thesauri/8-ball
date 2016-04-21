e84eefc44ea65b28eaa3b8c061f65ba374c506f2
Walter Berggren <walter.berggren@gmail.com>
2016-04-21 10:26:33 +0300

    Test for - vector operation

--- core/src/test/scala/Vector3DTest.scala
+++ core/src/test/scala/Vector3DTest.scala
@@ -16,6 +16,11 @@ class Vector3DTest extends FlatSpec with Matchers {
     v should be (expectedResult)
   }
   
+  "-" should "return the difference of two vectors" in {
+    val v = Vector3D(2f, 3f, -2f) - Vector3D(10f, 2f, 2f)
+    v should be (Vector3D(-8f, 1f, -4f))
+  }
+  
   "*" should "return the vector multiplied by a constant" in {
     (Vector3D(12f, 6f, 4f) * 0.25f) should be (Vector3D(3f, 1.5f, 1f))
   }
