b1d487366ceb4bbd3c89e2378eaf04fea8f9b4ac
Walter Berggren <walter.berggren@gmail.com>
2016-04-26 14:20:30 +0300

    Sort methods alphabetically

--- core/src/main/scala/PhysicsHandler.scala
+++ core/src/main/scala/PhysicsHandler.scala
@@ -115,57 +115,6 @@ object PhysicsHandler {
     balls foreach (ball => ball += t * ball.velocity)
   }
   
-  /** Updates the velocities of the given balls
-   *  
-   *  The logic is based on the equations from the following link:
-   *  http://archive.ncsa.illinois.edu/Classes/MATH198/townsend/math.html
-   *  
-   *  @param balls the balls to update
-   *  @param t time since last execution (in seconds) */
-  def updateVelocities(balls: Seq[Ball], t: Float): Unit = {
-    balls.foreach {
-      ball => {
-        
-        /* Determine if the ball is rolling or not, this is
-         * deemed to be the case if the relative velocity
-         * between the edge of the ball and the table is
-         * almost zero. */
-        
-        if (getRelativeVelocity(ball).norm <= 0.02f) {
-          
-          //Calculate the new velocity according to ∆v = -µg (v/|v|) ∆t
-          val newVelocity = ball.velocity + (-cfr * 9.8f * ball.velocity.normalized * t)
-          
-          //If both the velocity and the angular velocity are almost zero, stop the ball completely
-          if (ball.velocity.norm < 0.01f && ball.angularVelocity.norm < 0.01f ) {
-            ball.velocity = Vector3D(0f, 0f, 0f)
-            ball.angularVelocity = Vector3D(0f, 0f, 0f)
-            ball.state = BallState.Still
-          } else {
-            
-            //Otherwise update the velocities
-            ball.velocity = newVelocity
-            
-            //As the ball is rolling, the angular velocity should equal the velocity
-            ball.angularVelocity = Vector3D(-newVelocity.y / ball.radius, newVelocity.x / ball.radius, 0)
-          }  
-        } else {
-          
-          //--Sliding ball--
-          val pv = getRelativeVelocity(ball).normalized
-          
-          //Calculate the new velocity according to ∆v = -µg (v/|v|) ∆t
-          ball.velocity += -cfs * 9.8f * pv * t
-          
-          /* Calculate the new angular velocity using ∆ω = 5/2 (R x (-µmgr (v/|v|))) ∆t/(m*r^2)
-           * where R is a vector pointing from the middle of the ball to the touching point
-           * with the table (0,0,-r) */
-          ball.angularVelocity += (5f * t / (2f * ball.mass * pow(ball.radius.toDouble, 2).toFloat)) * (Vector3D(0f,0f,-ball.radius) cross (-cfs * ball.mass * g * ball.radius * pv))
-        }
-      }
-    }
-  }
-  
   /** Returns the time until the next collision between two balls
    *  
    *  The collision time is determined by treating the balls as if they were having a linear
@@ -332,6 +281,57 @@ object PhysicsHandler {
 
     applyCollisionsRecursive(t)
 
+  }
+
+  /** Updates the velocities of the given balls
+    *
+    *  The logic is based on the equations from the following link:
+    *  http://archive.ncsa.illinois.edu/Classes/MATH198/townsend/math.html
+    *
+    *  @param balls the balls to update
+    *  @param t time since last execution (in seconds) */
+  def updateVelocities(balls: Seq[Ball], t: Float): Unit = {
+    balls.foreach {
+      ball => {
+
+        /* Determine if the ball is rolling or not, this is
+         * deemed to be the case if the relative velocity
+         * between the edge of the ball and the table is
+         * almost zero. */
+
+        if (getRelativeVelocity(ball).norm <= 0.02f) {
+
+          //Calculate the new velocity according to ∆v = -µg (v/|v|) ∆t
+          val newVelocity = ball.velocity + (-cfr * 9.8f * ball.velocity.normalized * t)
+
+          //If both the velocity and the angular velocity are almost zero, stop the ball completely
+          if (ball.velocity.norm < 0.01f && ball.angularVelocity.norm < 0.01f ) {
+            ball.velocity = Vector3D(0f, 0f, 0f)
+            ball.angularVelocity = Vector3D(0f, 0f, 0f)
+            ball.state = BallState.Still
+          } else {
+
+            //Otherwise update the velocities
+            ball.velocity = newVelocity
+
+            //As the ball is rolling, the angular velocity should equal the velocity
+            ball.angularVelocity = Vector3D(-newVelocity.y / ball.radius, newVelocity.x / ball.radius, 0)
+          }
+        } else {
+
+          //--Sliding ball--
+          val pv = getRelativeVelocity(ball).normalized
+
+          //Calculate the new velocity according to ∆v = -µg (v/|v|) ∆t
+          ball.velocity += -cfs * 9.8f * pv * t
+
+          /* Calculate the new angular velocity using ∆ω = 5/2 (R x (-µmgr (v/|v|))) ∆t/(m*r^2)
+           * where R is a vector pointing from the middle of the ball to the touching point
+           * with the table (0,0,-r) */
+          ball.angularVelocity += (5f * t / (2f * ball.mass * pow(ball.radius.toDouble, 2).toFloat)) * (Vector3D(0f,0f,-ball.radius) cross (-cfs * ball.mass * g * ball.radius * pv))
+        }
+      }
+    }
   }
   
 }
