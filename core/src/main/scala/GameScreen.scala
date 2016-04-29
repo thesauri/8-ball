6921258e166bd002aacc067c46689736e908f3bd
Walter Berggren <walter.berggren@gmail.com>
2016-04-29 12:03:39 +0300

    GameScreen class to separate the game from the main menu

---
+++ core/src/main/scala/GameScreen.scala
@@ -0,0 +1,22 @@
+import com.badlogic.gdx.Screen
+
+/**
+  * Created by walter on 29/04/16.
+  */
+class GameScreen extends Screen {
+
+  override def hide(): Unit = ???
+
+  override def resize(width: Int, height: Int): Unit = ???
+
+  override def dispose(): Unit = ???
+
+  override def pause(): Unit = ???
+
+  override def render(delta: Float): Unit = ???
+
+  override def show(): Unit = ???
+
+  override def resume(): Unit = ???
+  
+}
