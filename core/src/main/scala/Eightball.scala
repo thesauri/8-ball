9fb827b99efc9b60e051cb991e436ed14f399626
Walter Berggren <walter.berggren@gmail.com>
2016-04-29 12:08:50 +0300

    Move game to GameScreen

--- core/src/main/scala/Eightball.scala
+++ core/src/main/scala/Eightball.scala
@@ -12,74 +12,15 @@ import scala.util.Random
 
 class Eightball extends Game {
 
-    lazy val scale = Gdx.graphics.getWidth / 3f //Scale factor for rendering
-    lazy val camera = new OrthographicCamera(Gdx.graphics.getWidth.toFloat, Gdx.graphics.getHeight.toFloat)
-    lazy val shapeRenderer = new ShapeRenderer()
-    lazy val gameBoard = new Board()
-    lazy val balls = Buffer[Ball]()
+  lazy val gameScreen = new GameScreen()
 
     override def create(): Unit = {
-      camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0f)
-      camera.update()
-      shapeRenderer.setProjectionMatrix(camera.combined)
-
-      //Cue ball
-      balls += new Ball(0.25f, 0.635f, 0f, 1)
-      balls(0).angularVelocity = Vector3D(0f, 0f, 0f)
-
-      balls += new Ball(1.69f, 0.635f, 0f, 1)
-
-      balls += new Ball(1.69f + 2.02f * balls(0).radius, 0.635f - 1.02f * balls(0).radius, 0f, 1)
-      balls += new Ball(1.69f + 2.02f * balls(0).radius, 0.635f + 1.02f * balls(0).radius, 0f, 1)
-
-      balls += new Ball(1.69f + 4.04f * balls(0).radius, 0.635f - 2.02f * balls(0).radius, 0f, 1)
-      balls += new Ball(1.69f + 4.04f * balls(0).radius, 0.635f, 0f, 1)
-      balls += new Ball(1.69f + 4.04f * balls(0).radius, 0.635f + 2.02f * balls(0).radius, 0f, 1)
-
-      balls += new Ball(1.69f + 6.06f * balls(0).radius, 0.635f - 3.03f * balls(0).radius, 0f, 1)
-      balls += new Ball(1.69f + 6.06f * balls(0).radius, 0.635f - 1.02f * balls(0).radius, 0f, 1)
-      balls += new Ball(1.69f + 6.06f * balls(0).radius, 0.635f + 1.02f * balls(0).radius, 0f, 1)
-      balls += new Ball(1.69f + 6.06f * balls(0).radius, 0.635f + 3.03f * balls(0).radius, 0f, 1)
-
-      balls += new Ball(1.69f + 8.08f * balls(0).radius, 0.635f - 4.04f * balls(0).radius, 0f, 1)
-      balls += new Ball(1.69f + 8.08f * balls(0).radius, 0.635f - 2.02f * balls(0).radius, 0f, 1)
-      balls += new Ball(1.69f + 8.08f * balls(0).radius, 0.635f, 0f, 1)
-      balls += new Ball(1.69f + 8.08f * balls(0).radius, 0.635f + 2.02f * balls(0).radius, 0f, 1)
-      balls += new Ball(1.69f + 8.08f * balls(0).radius, 0.635f + 4.04f * balls(0).radius, 0f, 1)
-
-      //Distribute balls a bit randomly
-      val random = new Random()
-      balls foreach { ball => {
-        ball += Vector3D(0.00005f * random.nextInt(100), 0.00005f * random.nextInt(100), 0f)
-      }}
-
-      for (i <- 0 until balls.size) {
-        for (n <- i + 1 until balls.size) {
-          if ((balls(i) - balls(n)).norm < balls(0).radius + balls(1).radius) println("Overlapping in the beginning")
-        }
-      }
-
-      ()
+      this.setScreen(gameScreen)
     }
 
     override def render(): Unit = {
-      Gdx.gl.glClearColor(1, 1, 1, 1);
-      Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-
-      if (Gdx.input.isTouched()) {
-        val random = new Random()
-        balls(0).velocity = Vector3D(random.nextFloat() * 5f, random.nextFloat() * 5f, 0f)
-      }
-      
-      PhysicsHandler.update(balls, Gdx.graphics.getDeltaTime)
-
-      shapeRenderer.begin(ShapeType.Filled)
-      gameBoard.render(shapeRenderer, scale)
-      balls.foreach(_.render(shapeRenderer, scale))
-      shapeRenderer.end()
+      gameScreen.render(Gdx.graphics.getDeltaTime())
     }
     
-    override def dispose(): Unit = {
-      shapeRenderer.dispose()
-    }
+    override def dispose(): Unit = ()
 }
--- core/src/main/scala/GameScreen.scala
+++ core/src/main/scala/GameScreen.scala
@@ -1,22 +1,92 @@
-import com.badlogic.gdx.Screen
+package com.walter.eightball
+
+import com.badlogic.gdx.graphics.{GL20, OrthographicCamera}
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType
+import com.badlogic.gdx.{Gdx, Screen}
+import com.walter.eightball.{Ball, Board, PhysicsHandler, Vector3D}
+
+import scala.collection.mutable.Buffer
+import scala.util.Random
 
 /**
   * Created by walter on 29/04/16.
   */
 class GameScreen extends Screen {
 
-  override def hide(): Unit = ???
+  lazy val scale = Gdx.graphics.getWidth / 3f //Scale factor for rendering
+  lazy val camera = new OrthographicCamera(Gdx.graphics.getWidth.toFloat, Gdx.graphics.getHeight.toFloat)
+  lazy val shapeRenderer = new ShapeRenderer()
+  lazy val gameBoard = new Board()
+  lazy val balls = Buffer[Ball]()
 
-  override def resize(width: Int, height: Int): Unit = ???
+  override def hide(): Unit = ()
 
-  override def dispose(): Unit = ???
+  override def resize(width: Int, height: Int): Unit = ()
 
-  override def pause(): Unit = ???
+  override def dispose(): Unit = ()
 
-  override def render(delta: Float): Unit = ???
+  override def pause(): Unit = ()
 
-  override def show(): Unit = ???
+  override def render(delta: Float): Unit = {
+    Gdx.gl.glClearColor(1, 1, 1, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-  override def resume(): Unit = ???
-  
+    if (Gdx.input.isTouched()) {
+      val random = new Random()
+      balls(0).velocity = Vector3D(random.nextFloat() * 5f, random.nextFloat() * 5f, 0f)
+    }
+
+    PhysicsHandler.update(balls, delta)
+
+    shapeRenderer.begin(ShapeType.Filled)
+    gameBoard.render(shapeRenderer, scale)
+    balls.foreach(_.render(shapeRenderer, scale))
+    shapeRenderer.end()
+  }
+
+  override def show(): Unit = {
+    camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0f)
+    camera.update()
+    shapeRenderer.setProjectionMatrix(camera.combined)
+
+    //Cue ball
+    balls += new Ball(0.25f, 0.635f, 0f, 1)
+    balls(0).angularVelocity = Vector3D(0f, 0f, 0f)
+
+    balls += new Ball(1.69f, 0.635f, 0f, 1)
+
+    balls += new Ball(1.69f + 2.02f * balls(0).radius, 0.635f - 1.02f * balls(0).radius, 0f, 1)
+    balls += new Ball(1.69f + 2.02f * balls(0).radius, 0.635f + 1.02f * balls(0).radius, 0f, 1)
+
+    balls += new Ball(1.69f + 4.04f * balls(0).radius, 0.635f - 2.02f * balls(0).radius, 0f, 1)
+    balls += new Ball(1.69f + 4.04f * balls(0).radius, 0.635f, 0f, 1)
+    balls += new Ball(1.69f + 4.04f * balls(0).radius, 0.635f + 2.02f * balls(0).radius, 0f, 1)
+
+    balls += new Ball(1.69f + 6.06f * balls(0).radius, 0.635f - 3.03f * balls(0).radius, 0f, 1)
+    balls += new Ball(1.69f + 6.06f * balls(0).radius, 0.635f - 1.02f * balls(0).radius, 0f, 1)
+    balls += new Ball(1.69f + 6.06f * balls(0).radius, 0.635f + 1.02f * balls(0).radius, 0f, 1)
+    balls += new Ball(1.69f + 6.06f * balls(0).radius, 0.635f + 3.03f * balls(0).radius, 0f, 1)
+
+    balls += new Ball(1.69f + 8.08f * balls(0).radius, 0.635f - 4.04f * balls(0).radius, 0f, 1)
+    balls += new Ball(1.69f + 8.08f * balls(0).radius, 0.635f - 2.02f * balls(0).radius, 0f, 1)
+    balls += new Ball(1.69f + 8.08f * balls(0).radius, 0.635f, 0f, 1)
+    balls += new Ball(1.69f + 8.08f * balls(0).radius, 0.635f + 2.02f * balls(0).radius, 0f, 1)
+    balls += new Ball(1.69f + 8.08f * balls(0).radius, 0.635f + 4.04f * balls(0).radius, 0f, 1)
+
+    //Distribute balls a bit randomly
+    val random = new Random()
+    balls foreach { ball => {
+      ball += Vector3D(0.00005f * random.nextInt(100), 0.00005f * random.nextInt(100), 0f)
+    }}
+
+    for (i <- 0 until balls.size) {
+      for (n <- i + 1 until balls.size) {
+        if ((balls(i) - balls(n)).norm < balls(0).radius + balls(1).radius) println("Overlapping in the beginning")
+      }
+    }
+  }
+
+  override def resume(): Unit = ()
+
 }
