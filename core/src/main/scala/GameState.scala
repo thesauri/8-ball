403f4086b5f1879fb277f742ac3e8544e29fe11b
Walter Berggren <walter.berggren@gmail.com>
2016-05-03 10:14:44 +0300

    Methods for returning the number of balls a player has remaining

--- core/src/main/scala/GameState.scala
+++ core/src/main/scala/GameState.scala
@@ -9,6 +9,20 @@ class GameState {
   var gameState = GameState.Aiming
   var hasSolids: Option[Int] = None //The player that shoots solids (fully colored balls), None, 1 or 2
   val cueStick = new CueStick(Vector3D(0f, 0f, 0f), 0f, 0f)
+
+  /** Returns the number of balls that player 1 has remaining */
+  def remainingBallsPlayer1: Int = hasSolids match {
+      case Some(1) => balls.count( _.number <= 8 )
+      case Some(2) => balls.count( _.number >= 8 )
+      case None => 8
+  }
+
+  /** Returns the number of balls that player 1 has remaining */
+  def remainingBallsPlayer2: Int = hasSolids match {
+    case Some(1) => balls.count( _.number >= 8 )
+    case Some(2) => balls.count( _.number <= 8 )
+    case None => 8
+  }
 }
 
 /** Describes the current state of the game
