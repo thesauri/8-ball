54868954161242767a23e3f1dc428f8fdd9ecc68
Walter Berggren <walter.berggren@gmail.com>
2016-05-04 16:14:08 +0300

    Add constructors for click events

--- core/src/main/scala/SInputListeners.scala
+++ core/src/main/scala/SInputListeners.scala
@@ -1,10 +1,31 @@
 package com.walter.eightball
 
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.badlogic.gdx.scenes.scene2d.{InputEvent, InputListener}
 
 /** Scala-like input listeners. Instead of implementing an interface for input events,
   * simply pass the function to be executed on the desired event  */
 object SInputListeners {
+
+  def click(f: => Unit): ClickListener = {
+
+    class cListener extends ClickListener {
+      override def clicked(event: InputEvent, x: Float, y: Float): Unit = f
+    }
+
+    new cListener
+
+  }
+
+  def click(f: (InputEvent) => Unit): ClickListener = {
+
+    class cListener extends ClickListener {
+      override def clicked(event: InputEvent, x: Float, y: Float): Unit = f(event)
+    }
+
+    new cListener
+
+  }
 
   def touchDown(f: => Boolean): InputListener = {
 
