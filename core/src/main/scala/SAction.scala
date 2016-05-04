38f5d5025b6491a961b763a334fd22fc22c88f0c
Walter Berggren <walter.berggren@gmail.com>
2016-05-04 15:31:54 +0300

    Scala-like Action object

---
+++ core/src/main/scala/SAction.scala
@@ -0,0 +1,15 @@
+package com.walter.eightball
+
+import com.badlogic.gdx.scenes.scene2d.Action
+
+/** Scala-like libgdx Action. Works by passing a function literal to be executed every act(deltaTime) */
+object SAction {
+
+  class SActionImplementation(f: (Float) => Boolean) extends Action {
+    def act(delta: Float): Boolean = f(delta)
+  }
+
+  def apply(f: => Boolean): Action = new SActionImplementation( (t: Float) => f )
+  def apply(f: (Float) => Boolean): Action = new SActionImplementation( f )
+
+}
