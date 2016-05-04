129b18b1980740be1b62607599a4bd66cf5af464
Walter Berggren <walter.berggren@gmail.com>
2016-05-04 10:39:13 +0300

    Empty main menu screen

---
+++ core/src/main/scala/MenuScreen.scala
@@ -0,0 +1,20 @@
+package com.walter.eightball
+
+import com.badlogic.gdx.Screen
+
+class MenuScreen extends Screen {
+
+  def show(): Unit = ()
+
+  def render(delta: Float): Unit = ()
+
+  def resize(width: Int, height: Int): Unit = ()
+
+  def dispose(): Unit = ()
+
+  def hide(): Unit = ()
+
+  def pause(): Unit = ()
+
+  def resume(): Unit = ()
+}
